9ad694e91550d83549447457f38f60729aa3d601
casperwitting <casper.witting@gmail.com>
2020-03-27 08:45:49 +0100

    Removed cassandra extension

--- app/Patterns/Proxy/GitlabRepositoryClient.php
+++ app/Patterns/Proxy/GitlabRepositoryClient.php
@@ -14,17 +14,17 @@ class GitlabRepositoryClient
         $this->client = $client;
     }
 
-    public function getBranchesForRepository(int $id)
+    public function getBranchesForRepository(int $id): string
     {
         return $this->client->get('/repositories/' . $id . '/branches');
     }
 
-    public function getCommitsForRepository(int $id)
+    public function getCommitsForRepository(int $id): string
     {
         return $this->client->get('/repositories/' . $id . '/commits');
     }
 
-    public function getFilesForRepository(int $id)
+    public function getFilesForRepository(int $id): string
     {
         return $this->client->get('/repositories/' . $id . '/files');
     }
